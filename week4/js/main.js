46a346e449a2425c26d3ca6f7276541db565fac5
unknown <kshitijnairr@gmail.com>
2022-10-03 18:49:04 -0700

    added for and while loop

---
+++ week4/js/main.js
@@ -0,0 +1,22 @@
+let power = 0;
+
+// for-loop block
+console.log("Output from for-loop:");
+for(let i = 0; power < 1024; i++) {
+    power = Math.pow(2, i);
+    console.log(power);
+}
+
+// while-loop block
+power = 0;
+
+let i = 0;
+
+console.log("Output from while-loop:");
+while(power < 1024) {
+    power = Math.pow(2, i);
+    i++;
+    console.log(power);
+}
+
+// do-while loop block
