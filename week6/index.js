1394566136529ff41ada0ffd8a4fb5714688190b
unknown <kshitijnairr@gmail.com>
2022-10-17 17:35:08 -0700

    fs.writeFile() function was addedd and is working

--- week6/index.js
+++ week6/index.js
@@ -1,3 +1,14 @@
-require('express')
+const fs = require('fs')
 
-console.log("App is running from index.js! :D ")
+// standard dataype for text file UTF-8
+const string = "Hi! This is a new file that was created from the Node.js fs module :D"
+
+const data = new Uint8Array(Buffer.from(string));
+
+fs.writeFile('newFile.txt', data, (err) => {
+    if (err) {
+        console.log(`Error: ${err}`)
+    }
+    console.log("The new file was created succesfully! :)");
+})
+
