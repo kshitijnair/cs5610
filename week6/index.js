0c1742f6c88cd6fe16f18ac7c35d62b02a39ed08
unknown <kshitijnairr@gmail.com>
2022-10-17 17:38:11 -0700

    fs.readFile() method was added and is working properly

--- week6/index.js
+++ week6/index.js
@@ -6,9 +6,11 @@ const string = "Hi! This is a new file that was created from the Node.js fs modu
 const data = new Uint8Array(Buffer.from(string));
 
 fs.writeFile('newFile.txt', data, (err) => {
-    if (err) {
-        console.log(`Error: ${err}`)
+    if (err) console.log(`Error: ${err}`);
+    else {
+        fs.readFile('newFile.txt', (err, data) => {
+            if(err) console.log(`Error: ${err}`);
+            console.log(`This is the data from the new file: ${data}`)
+        })
     }
-    console.log("The new file was created succesfully! :)");
 })
-
